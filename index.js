d396eba4cc1a6576fe1d887ca15b7c36c03039a3
liangxingzhi <liangxingzhi@ud-deploy01v.ops.corp.qihoo.net>
2016-03-09 20:41:33 +0800

    逻辑处理

--- index.js
+++ index.js
@@ -26,25 +26,12 @@ function init(site) {
 //用request获取rss
 function getRSS(context) {
     return new Promise((resolve, reject) => {
-         request(context.site.url, {timeout: 5000}, (error, response, body) => {
+        request(context.site.url, {timeout: 5000}, (error, response, body) => {
             if (error) {
                 reject(error);
                 return;
             }
             context.body = body;
-            resolve(context);
-        });
-    });
-}
-
-//标记网站访问时间
-function setLastTime(context) {
-    context.site.lastTime = +new Date();
-    return new Promise((resolve, reject) => {
-        fs.writeFile(configFile, JSON.stringify(config, null, 4), err => {
-            if (err) {
-                return reject(err);
-            }
             resolve(context);
         });
     });
@@ -66,11 +53,24 @@ function parseXml (context) {
 
 //根据发布时间筛选出符合要求的内容
 function filterArticles (context) {
-    console.log(config);
+    //console.log(config);
     var lastTime = context.site.lastTime || (+new Date() - 3600*24*7*1000);
-    console.log(lastTime);
+    //console.log(lastTime);
     context.articles = context.articles.filter(article => +new Date(article.published[0]) > lastTime);
     return context;
+}
+
+//标记网站访问时间
+function setLastTime(context) {
+    context.site.lastTime = +new Date();
+    return new Promise((resolve, reject) => {
+        fs.writeFile(configFile, JSON.stringify(config, null, 4), err => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(context);
+        });
+    });
 }
 
 //将符合要求的内容发送到文章推荐的接口
